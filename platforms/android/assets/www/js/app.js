7f6ba1515bad2e093bc1ffb12df59efc47cce3fd
aneeshverenkar <aneesh.verenkar@gmail.com>
2016-03-13 17:37:45 -0500

    added android platform

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,22 +3,67 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic' , 'starter.controllers', 'starter.services', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.config(function($stateProvider, $urlRouterProvider) {
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+    })
+
+
+  // Each tab has its own nav history stack:
+
+      .state('tab.profile', {
+        url: '/profile',
+        views: {
+          'tab-profile': {
+            templateUrl: 'templates/tab-profile.html',
+            controller: 'ProfileCtrl'
+          }
+        }
+      })
+
+      .state('tab.view', {
+        url: '/view',
+        views: {
+          'tab-view': {
+            templateUrl: 'templates/tab-view.html',
+            controller: 'ViewCtrl'
+          }
+        }
+      })
+
+      .state('tab.view-detail', {
+      url: '/view/:viewId',
+      views: {
+        'tab-view': {
+          templateUrl: 'templates/view-detail.html',
+          controller: 'ViewDetailCtrl'
+        }
+      }
+    })
+
+    .state('tab.create', {
+    url: '/create',
+    views: {
+      'tab-create': {
+        templateUrl: 'templates/tab-create.html',
+        controller: 'CreateCtrl'
+      }
     }
   });
-})
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/view');   
+
+
+});  
+
+
+
