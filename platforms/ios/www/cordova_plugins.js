ddfad2a8f42c874574f74cab468a429c5b651be2
Ceren Kaplan <cerenkaplan@pcp352142pcs.unl.edu>
2016-03-13 16:05:19 -0500

    login

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,8 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
         "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
         "id": "cordova-plugin-console.console",
+        "pluginId": "cordova-plugin-console",
         "clobbers": [
             "console"
         ]
@@ -10,6 +20,7 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-console/www/logger.js",
         "id": "cordova-plugin-console.logger",
+        "pluginId": "cordova-plugin-console",
         "clobbers": [
             "cordova.logger"
         ]
@@ -17,26 +28,19 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
         "clobbers": [
             "navigator.splashscreen"
         ]
-    },
-    {
-        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "ionic-plugin-keyboard": "1.0.8",
     "cordova-plugin-console": "1.0.2",
     "cordova-plugin-splashscreen": "3.1.0",
-    "cordova-plugin-whitelist": "1.2.1",
-    "ionic-plugin-keyboard": "1.0.8"
+    "cordova-plugin-whitelist": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,7 +12,35 @@ angular.module('starter.controllers', [])
   $scope.view = Petitions.get($stateParams.viewId);
 })
 */
+
 .controller('CreateCtrl', function($scope) {});
+
+
+/*
+.controller('LoginCtrl', function($scope, $firebaseAuth, $location) {
+
+	$scope.login = function(email, password){
+		var fbAuth = $firebaseAuth(fb);
+		fbAuth.$authWithPassword({
+			email : email,
+			password : password
+		}).then(function(authData) {
+				$location.path("/view");
+		}).catch(function(error){
+			alert("Error: " + error);
+		}); 	
+
+	}
+
+	$scope.register = function(){
+		var fbAuth = $firebaseAuth(fb);
+		fbAuth.$createUser().then(function(){
+			return fbAuth.$
+		}
+	}
+
+});*/
+
 
 
 
